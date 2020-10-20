25b0faa4e63ba7bb0f427c96f77ea5eef17de51b
jamalamch <52598201+jamalamch@users.noreply.github.com>
2020-10-21 00:16:34 +0100

    make image dinamic

--- src/js/asciiart_stillimage.js
+++ src/js/asciiart_stillimage.js
@@ -5,36 +5,62 @@ var myAsciiArt;
 */
 var asciiart_width = 122; var asciiart_height = 64;
 
-var images ;
+var imageAscii;
 
 var gfx;
 
 var ascii_arr;
 
-var cyclic_t;
 /*
   Let's load the example images first.
 */
 function preload() {
-  images = loadImage('src/assets/CROPPED-DSC_4999.jpg');
+  imageAscii = loadImage('src/assets/CROPPED-DSC_4999.jpg');
 }
 
 function setup() {
   gfx = createGraphics(asciiart_width, asciiart_height);
   gfx.pixelDensity(1);
   myAsciiArt = new AsciiArt(this);
-  myAsciiArt.printWeightTable();
-  gfx.image(images, 0, 0, gfx.width, gfx.height);
+  convertImage();
+}
+
+function convertImage() {
+  gfx.image(imageAscii, 0, 0, gfx.width, gfx.height);
   gfx.filter(POSTERIZE, 3);
   ascii_arr = myAsciiArt.convert(gfx);
-  putTextImage();
-}
-function putTextImage(){
-  document.getElementById('image-string').innerHTML=myAsciiArt.convert2dArrayToString(ascii_arr);
+
+  document.getElementById('image-string').innerHTML = myAsciiArt.convert2dArrayToString(ascii_arr);
 }
 
+var dataImage;
+var recharge = false;
 
-function mouseReleased() {
-  console.log(myAsciiArt.convert2dArrayToString(ascii_arr));
+document.getElementById('file-image').onchange = function () {
+  if (this.files && this.files.length) {
+    var fr = new FileReader();
+    fr.onload = function () {
+      dataImage = fr.result;
+      recharge = true;
+      document.getElementById('image-origin').src = dataImage;
+    }
+    fr.readAsDataURL(this.files[0]);
+  }
+}
+
+function reloadImage() {
+  if (recharge && dataImage) {
+    recharge = false;
+    loadImage(dataImage,
+      img => {
+        imageAscii = img;
+        convertImage()
+      }
+      ,
+      function () {
+        console.log("not load");
+      }
+    );
+  }
 }
 
